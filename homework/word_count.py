ddc38edbde099f5c2375407ba52d58314bbcc3f4
intento1 <deespanaj@unal.edu.co>
2025-09-23 23:44:12 -0500

    int1

--- homework/word_count.py
+++ homework/word_count.py
@@ -1,82 +1,119 @@
 """Taller evaluable"""
 
-# pylint: disable=broad-exception-raised
+  # pylint: disable=broad-exception-raised
 
 import fileinput
 import glob
 import os.path
 import time
 from itertools import groupby
+import string
 
 from toolz.itertoolz import concat, pluck
 
 
-
 def copy_raw_files_to_input_folder(n):
-    """Generate n copies of the raw files in the input folder"""
+      """Generate n copies of the raw files in the input folder"""
 
+      create_directory("files/input")
 
+      for file in glob.glob("files/raw/*"):
+            
+            with open(file, "r", encoding="utf-8") as f:
+                  text = f.read()
 
+            for i in range(1, n + 1):
+                  filename = f"{os.path.basename(file).split('.')[0]}_{i}.txt"
+                  with open(f"files/input/{filename}", "w", encoding="utf-8") as f2:
+                        f2.write(text)
+
+   
 def load_input(input_directory):
-    """Funcion load_input"""
+      """Funcion load_input"""
+
+      sequence = []
+      files = glob.glob(f"{input_directory}/*")
+      with fileinput.input(files=files) as f:
+            for line in f:
+                  sequence.append((fileinput.filename(), line))
+      return sequence
 
 
 def preprocess_line(x):
-    """Preprocess the line x"""
+      """Preprocess the line x"""
+      text = x[1]
+      text = text.lower()
+      text = text.translate(str.maketrans("", "", string.punctuaction))
+      text = text.replace("\n","")
+      return (x[0], text)
 
 
 def map_line(x):
-    pass
+      x = preprocess_line(x)
+      x = x[1].slipt()
+      x = [(w, 1) for w in x[1].split()]
+      return x
+
 
 def mapper(sequence):
-    """Mapper"""
+      """Mapper"""
+      sequence = map(map_line, sequence)
+      sequence = concat(sequence)
+      return sequence
 
 
 def shuffle_and_sort(sequence):
-    """Shuffle and Sort"""
-
+      """Shuffle and Sort"""
 
 
 def compute_sum_by_group(group):
-    pass
+      pass
+
 
 def reducer(sequence):
-    """Reducer"""
+      """Reducer"""
 
 
 def create_directory(directory):
-    """Create Output Directory"""
+      """Create Output Directory"""
+
+      if os.path.exists(directory):
+          for file in glob.glob(f"{directory}/*"):
+              os.remove(file)
+          os.rmdir(directory)
+
+      os.makedirs(directory)
 
 
 def save_output(output_directory, sequence):
-    """Save Output"""
+      """Save Output"""
 
 
 def create_marker(output_directory):
-    """Create Marker"""
+      """Create Marker"""
 
 
 def run_job(input_directory, output_directory):
-    """Job"""
-    sequence = load_input(input_directory)
-    sequence = mapper(sequence)
-    sequence = shuffle_and_sort(sequence)
-    sequence = reducer(sequence)
-    create_directory(output_directory)
-    save_output(output_directory, sequence)
-    create_marker(output_directory)
+      """Job"""
+      sequence = load_input(input_directory)
+      sequence = mapper(sequence)
+      sequence = shuffle_and_sort(sequence)
+      sequence = reducer(sequence)
+      create_directory(output_directory)
+      save_output(output_directory, sequence)
+      create_marker(output_directory)
 
 
 if __name__ == "__main__":
 
-    copy_raw_files_to_input_folder(n=1000)
+      copy_raw_files_to_input_folder(n=1000)
 
-    start_time = time.time()
+      start_time = time.time()
 
-    run_job(
-        "files/input",
-        "files/output",
-    )
+      run_job(
+          "files/input",
+          "files/output",
+      )
 
-    end_time = time.time()
-    print(f"Tiempo de ejecución: {end_time - start_time:.2f} segundos")
+      end_time = time.time()
+      print(f"Tiempo de ejecución: {end_time - start_time:.2f} segundos")
